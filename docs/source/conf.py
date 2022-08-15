6e24b0c55a58350a318205e8673810ee561a0fa4
OchaBanana <55866611+OchaBanana@users.noreply.github.com>
2022-08-15 10:48:01 +0700

    commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Nutanix Move'
-copyright = '2022, Toh'
+copyright = '2022, Nutanix'
 author = 'Toh'
-release = '0,1'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,8 +25,21 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # import sphinx_rtd_theme
-
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'pyramid'
+import sphinx_bootstrap_theme
+import sphinx_fontawesome
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_static_path = ['_static']
+extensions = ['sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinxcontrib.fulltoc',
+    'sphinx_fontawesome']
 
-html_static_path = ['_static']
+html_logo = "_static/NutanixWorkshops.svg"
+
+html_favicon = "_static/favicon.ico"
+
+html_title = ""
+
+html_show_sphinx = False
